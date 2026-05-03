01e1447ee531cbe408546b500a35452997e08645
fredrexx <federicomariano75@gmail.com>
2026-05-03 07:22:50 +0200

    Update c.py

--- c.py
+++ c.py
@@ -50,7 +50,12 @@ def update(M):
     x = r*np.cos(phi)
     y = r*np.sin(phi)
     z = 2*np.sqrt(Rs*(r-Rs))
-    ax.plot(x,y,z)
+    r2 = np.linspace(Rs, 100, 200)
+    phi2 = np.linspace(0, 2*np.pi, 200)
+    x2 = r*np.cos(phi2)
+    y2 = r*np.sin(phi2)
+    z2 = 2*np.sqrt(Rs*(r2-Rs))
+    ax.plot(x2,y2,z2)
     #axes limits
     ax.set_xlim(-100, 100)
     ax.set_ylim(-100, 100)
